765d3459513f38d46646f088abf240c9aa101fa3
Lucas Lima <lucasgabrieljake@gmail.com>
2025-07-09 00:20:16 -0300

    feat: rsa and pss tests

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from RSA import RSA
+from RSA_PSS import RSAPSS
 
-rsa = RSA()
 
-x = rsa.encrypt(450)
-print(x, end="\n\n")
+if __name__ == "__main__":
+    rsa = RSA()
+    print("RSA TESTE")
+    x = rsa.encrypt(450)
+    print(f"cipher : {x}", end="\n\n")
 
-print(rsa.decrypt(x))
+    print(f"plain: {rsa.decrypt(x)}")
+    print("--------\n")
+    m = b"Fluminense Futebol Clube"
+    fake_m = b"Fuluminense Futebol Clube"
+
+    pss = RSAPSS(salt_len=32)
+
+    signature, salt = pss.sign(m)
+    print("Assinatura:", signature)
+    print("Salt:", salt.hex())
+
+    valid = pss.verify(m, signature, salt)
+    print("is valid?", valid)
+
+    invalid = pss.verify(fake_m, signature, salt)
+    print("is valid?:", invalid)
